873a85cc60b2833cbc52937487bfebccd92cceb5
Abdullah <abdullahkhan221121@gmail.com>
2026-04-08 18:27:03 +0500

    forth commit

--- app/main.py
+++ app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from app.db.database import engine
 from app.db.base import Base 
 from app.api.routes import user
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 #Create tables
 Base.metadata.create_all(bind=engine)
